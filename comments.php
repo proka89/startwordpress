d641e1e4354070f91d91dc5026bef38b33f44146
proka89 <vukovicjovan89@gmail.com>
2018-01-14 16:05:03 +0100

    files

--- comments.php
+++ comments.php
@@ -2,12 +2,12 @@
 	return;
 }
 ?>
-	<div id="comments" class="comments-area">
-		<?php if ( have_comments() ) : ?>
-			<h3 class="comments-title">
-				<?php
-				printf( _nx( 'One comment on “%2$s”', '%1$s comments on “%2$s”', get_comments_number(), 'comments title'),
-					number_format_i18n( get_comments_number() ), get_the_title() );
+<div id="comments" class="comments-area">
+	<?php if ( have_comments() ) : ?>
+		<h3 class="comments-title">
+			<?php
+			printf( _nx( 'One comment on “%2$s”', '%1$s comments on “%2$s”', get_comments_number(), 'comments title'),
+				number_format_i18n( get_comments_number() ), get_the_title() );
 				?>
 			</h3>
 			<ul class="comment-list">
@@ -15,14 +15,14 @@
 				wp_list_comments( array(
 					'short_ping'  => true,
 					'avatar_size' => 50,
-				) );
-				?>
-			</ul>
-		<?php endif; ?>
-		<?php if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-			<p class="no-comments">
-				<?php _e( 'Comments are closed.' ); ?>
-			</p>
-		<?php endif; ?>
-		<?php comment_form(); ?>
-	</div>
+					) );
+					?>
+				</ul>
+			<?php endif; ?>
+			<?php if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+				<p class="no-comments">
+					<?php _e( 'Comments are closed.' ); ?>
+				</p>
+			<?php endif; ?>
+			<?php comment_form(); ?>
+		</div>
--- functions.php
+++ functions.php
@@ -10,9 +10,9 @@ add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
 
 // Add Google Fonts
 function startwordpress_google_fonts() {
-				wp_register_style('OpenSans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
-				wp_enqueue_style( 'OpenSans');
-		}
+  wp_register_style('OpenSans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
+  wp_enqueue_style( 'OpenSans');
+}
 
 add_action('wp_print_styles', 'startwordpress_google_fonts');
 
@@ -27,35 +27,35 @@ add_action( 'admin_menu', 'custom_settings_add_menu' );
 
 // Create Custom Global Settings
 function custom_settings_page() { ?>
-  <div class="wrap">
-    <h1>Custom Settings</h1>
-    <form method="post" action="options.php">
-       <?php
-           settings_fields( 'section' );
-           do_settings_sections( 'theme-options' );
-           submit_button();
-       ?>
-    </form>
-  </div>
+<div class="wrap">
+  <h1>Custom Settings</h1>
+  <form method="post" action="options.php">
+   <?php
+   settings_fields( 'section' );
+   do_settings_sections( 'theme-options' );
+   submit_button();
+   ?>
+ </form>
+</div>
 <?php }
 
 // Twitter
 function setting_twitter() { ?>
-  <input type="text" name="twitter" id="twitter" value="<?php echo get_option( 'twitter' ); ?>" />
+<input type="text" name="twitter" id="twitter" value="<?php echo get_option( 'twitter' ); ?>" />
 <?php }
 
 function custom_settings_page_setup() {
   add_settings_section( 'section', 'All Settings', null, 'theme-options' );
   add_settings_field( 'twitter', 'Twitter URL', 'setting_twitter', 'theme-options', 'section' );
   add_settings_field( 'github', 'GitHub URL', 'setting_github', 'theme-options', 'section' );
-	register_setting( 'section', 'twitter' );
+  register_setting( 'section', 'twitter' );
   register_setting( 'section', 'github' );
 }
 add_action( 'admin_init', 'custom_settings_page_setup' );
 
 //GitHub
 function setting_github() { ?>
-  <input type="text" name="github" id="github" value="<?php echo get_option('github'); ?>" />
+<input type="text" name="github" id="github" value="<?php echo get_option('github'); ?>" />
 <?php }
 
 // Support Featured Images
