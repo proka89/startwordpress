3f24036ff8200cfb161262e3fa5f3dd76ded4d61
proka89 <vukovicjovan89@gmail.com>
2018-01-14 16:21:13 +0100

    files

--- functions.php
+++ functions.php
@@ -10,8 +10,8 @@ add_action( 'wp_enqueue_scripts', 'startwordpress_scripts' );
 
 // Add Google Fonts
 function startwordpress_google_fonts() {
-  wp_register_style('OpenSans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
-  wp_enqueue_style( 'OpenSans');
+ wp_register_style('OpenSans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
+ wp_enqueue_style( 'OpenSans');
 }
 
 add_action('wp_print_styles', 'startwordpress_google_fonts');
@@ -21,20 +21,20 @@ add_theme_support( 'title-tag' );
 
 // Custom settings
 function custom_settings_add_menu() {
-  add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99 );
+ add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99 );
 }
 add_action( 'admin_menu', 'custom_settings_add_menu' );
 
 // Create Custom Global Settings
 function custom_settings_page() { ?>
 <div class="wrap">
-  <h1>Custom Settings</h1>
-  <form method="post" action="options.php">
-   <?php
-   settings_fields( 'section' );
-   do_settings_sections( 'theme-options' );
-   submit_button();
-   ?>
+ <h1>Custom Settings</h1>
+ <form method="post" action="options.php">
+  <?php
+  settings_fields( 'section' );
+  do_settings_sections( 'theme-options' );
+  submit_button();
+  ?>
  </form>
 </div>
 <?php }
@@ -45,11 +45,11 @@ function setting_twitter() { ?>
 <?php }
 
 function custom_settings_page_setup() {
-  add_settings_section( 'section', 'All Settings', null, 'theme-options' );
-  add_settings_field( 'twitter', 'Twitter URL', 'setting_twitter', 'theme-options', 'section' );
-  add_settings_field( 'github', 'GitHub URL', 'setting_github', 'theme-options', 'section' );
-  register_setting( 'section', 'twitter' );
-  register_setting( 'section', 'github' );
+ add_settings_section( 'section', 'All Settings', null, 'theme-options' );
+ add_settings_field( 'twitter', 'Twitter URL', 'setting_twitter', 'theme-options', 'section' );
+ add_settings_field( 'github', 'GitHub URL', 'setting_github', 'theme-options', 'section' );
+ register_setting( 'section', 'twitter' );
+ register_setting( 'section', 'github' );
 }
 add_action( 'admin_init', 'custom_settings_page_setup' );
 
@@ -60,4 +60,4 @@ function setting_github() { ?>
 
 // Support Featured Images
 add_theme_support( 'post-thumbnails' );
-//
+
